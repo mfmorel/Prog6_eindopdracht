65bb55262da8d3f6ccc4c0f0052921c052cd9e83
mfmorel <mfmorel@avans.nl>
2018-01-12 15:18:13 +0100

    Add project files.

---
+++ Prog6/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Prog6.Startup))]
+namespace Prog6
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
